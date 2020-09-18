a7bb671721042d706eb223b37e09072848c47c00
Franco Pizzutti <fpizzuttimed@gmail.com>
2020-09-18 20:19:34 -0300

    First stable version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ISW_TP_2020
-ISW TRABAJO PRACTICO 
+## ISW TP
+
+The aim of this project is to understand gitflow and approve the signature
+
+### Integrants
+- Pizzutti Franco
+- Garcia Aguirre Gonzalo
+- Lozano Indiana
+- Grepi Nicolas
+
+### To run
+node server.js
---
+++ api/routes/products.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const { response } = require('express');
+const router = express.Router();
+
+
+/**
+ * Handles common GET request
+ * @return json response
+ */
+router.get('/',(request, response, nextFunction) => {
+    response.status(200).json({
+        message: 'Handling GET requests to /products endpoint'
+    });
+});
+
+/**
+ * Handles common POST request
+ * @returns json response
+ */
+router.post('/',(request, response, nextFunction) => {
+    response.status(200).json({
+        message: 'Handling POST requests to /products endpoint'
+    });
+});
+
+/**
+ * Handles getByProductId GET request
+ * @returns json response
+ */
+router.get('/:productId', (request, response, nextFunction) => {
+    const id = request.params.productId;
+    switch(id) {
+        case 'shoes': response.status(200).json({
+            message: 'Handling GET request for a SHOE product'
+        });
+        break;
+        case 'sweaters': response.status(200).json({
+            message: 'Handling GET request for a SWEATER product'
+        });
+        break;
+    }
+});
+
+/**
+ * Handles deteleByProductId DELETE request
+ * @returns json response
+ */
+router.delete('/:productId', (request, response, nextFunction) => {
+    var id = request.params.productId;
+    switch(id) {
+        case 'shoes': response.status(200).json({
+            message: 'Handling DELETE request to delete a SHOE product'
+        });
+        break;
+        case 'sweaters': response.status(200).json({
+            message: 'Handling DELETE request to delete a SWEATER product'
+        });
+        break;
+    }
+});
+
+/**
+ * Handles updateById PUT request
+ * @returns json response
+ */
+router.put('/:productId', (request, response, nextFunction) => {
+    var id = request.params.productId;
+    switch(id) {
+        case 'shoes': response.status(200).json({
+            message: 'Handling PUT request to update a SHOE product'
+        });
+        break;
+        case 'sweaters': response.status(200).json({
+            message: 'Handling PUT request to update a SWEATER product'
+        });
+        break;
+    }
+});
+
+module.exports = router;
